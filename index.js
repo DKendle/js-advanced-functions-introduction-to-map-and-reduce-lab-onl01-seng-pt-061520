bfffa39af76f83f0ba4ab0e1971e269fcc33ac8f
DKendle <dina.kendle@gmail.com>
2021-01-18 13:21:42 -0500

    commit tree c044581856657d6a7f5da97a47acbfe5bdde2fe7

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Your code here
+function mapToNegativize(array) {
+    return array.map(i => i * -1)
+}
+
+function mapToNoChange(array){
+    return array
+}
+
+function mapToDouble(array){
+    return array.map(i => i*2)
+}
+ function mapToSquare(array){
+     return array.map(i => i ** 2)
+ }
+
+ function reduceToTotal(array, start){
+     return array.reduce(array, start)
+ }
